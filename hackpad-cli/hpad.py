716120add45ee448089c916c8dfdd667b7388d71
Octavian <ccraciun@gmail.com>
2015-12-27 19:16:30 -0800

    Add some class.

--- hackpad-cli/hpad.py
+++ hackpad-cli/hpad.py
@@ -19,10 +19,6 @@ Options:
     --revision=<revision>       Pad revision id [default: latest]
     --format=<format>           Pad format (html|md|native|txt) [default: md]
 """
-import json
-import pprint
-
-from docopt import docopt
 from requests_oauthlib import OAuth1Session
 
 
@@ -35,76 +31,112 @@ CONTENT_TYPE_MAP = {
         'md': 'text/x-web-markdown',
         }
 
+
+class HackpadSession(object):
+    def __init__(self, key, secret, url='https://hackpad.com/'):
+        self.url = url
+        self.oauth_session = OAuth1Session(key, secret)
+        self.api_endpoint = url + API_PATH
+
+    def get(self, url, *args, **kwargs):
+        print('GET', url, args, kwargs)
+        return self.oauth_session.get(url, *args, **kwargs)
+
+    def post(self, url, *args, **kwargs):
+        print('POST', url, args, kwargs)
+        return self.oauth_session.post(url, *args, **kwargs)
+
+    def pad_list(self):
+        req_url = self.api_endpoint + 'pads/all'
+        return self.get(req_url)
+
+    def pad_create(self, data, data_format='md'):
+        req_url = self.api_endpoint + 'pad/create'
+        req_headers = {CONTENT_TYPE_HEADER: CONTENT_TYPE_MAP[data_format]}
+        return self.post(req_url, data, headers=req_headers)
+
+    def pad_get(self, pad_id, data_format='md', revision='latest'):
+        req_url = self.api_endpoint + 'pad/' + pad_id + '/content/'
+        req_url += str(revision)
+        req_url += '.' + data_format
+        return self.get(req_url)
+
+    def pad_put(self, pad_id, data, data_format='md'):
+        req_url = self.api_endpoint + 'pad/' + pad_id + '/content'
+        req_headers = {CONTENT_TYPE_HEADER: CONTENT_TYPE_MAP[data_format]}
+        return self.post(req_url, data, headers=req_headers)
+
+    def pad_revert(self, pad_id, revision):
+        req_url = self.api_endpoint + 'pad/' + pad_id + '/revert-to/' + revision
+        return self.post(req_url)
+
+    def pad_revisions(self, pad_id):
+        req_url = self.api_endpoint + 'pad/' + pad_id + '/revisions'
+        return self.get(req_url)
+
+    def pad_get_options(self, pad_id):
+        req_url = self.api_endpoint + 'pad/' + pad_id + '/options'
+        return self.get(req_url)
+
+    def pad_set_option(self, pad_id, setting, value):
+        req_url = self.api_endpoint + 'pad/' + pad_id + '/options'
+        req_params = {setting: value}
+        return self.post(req_url, params=req_params)
+
+
 def main():
+    import json
+    import pprint
+
+    from docopt import docopt
     arguments = docopt(__doc__, version='hpad.py 0.1')
     if arguments['--config']:
         arguments.update(json.load(open(arguments['--config'])))
 
-    api_endpoint = arguments['--url'] + API_PATH
+    hackpad_session = HackpadSession(arguments['--key'], arguments['--secret'],
+                                     url=arguments['--url'])
 
-    oauth_session = OAuth1Session(arguments['--key'], client_secret=arguments['--secret'])
+    if arguments['pad'] and (arguments['create'] or arguments['put']):
+        if arguments['<file>'] == '-':
+            stream = sys.stdin
+        else:
+            stream = open(arguments['<file>'])
 
     if arguments['pad']:
         if arguments['list']:
-            print(api_endpoint + 'pads/all')
-            print(oauth_session.get(api_endpoint + 'pads/all').json())
+            resp = hackpad_session.pad_list()
+            pprint.pprint(resp.json())
 
         if arguments['create']:
-            req_url = api_endpoint + 'pad/create'
-            if arguments['<file>'] == '-':
-                f = sys.stdin
-            else:
-                f = open(arguments['<file>'])
-            req_headers = {CONTENT_TYPE_HEADER: CONTENT_TYPE_MAP[arguments['--format']]}
-            print(req_url)
-            print(req_headers)
-            resp = oauth_session.post(req_url, f.read(), headers=req_headers)
+            resp = hackpad_session.pad_create(stream.read(), data_format=arguments['--format'])
             pprint.pprint(resp.json())
 
         if arguments['get']:
-            req_url = api_endpoint + 'pad/' + arguments['<pad_id>'] + '/content/'
-            if arguments['--revision']:
-                req_url += arguments['--revision']
-            if arguments['--format']:
-                req_url += '.' + arguments['--format']
-            print(req_url)
-            pprint.pprint(oauth_session.get(req_url).content)
+            resp = hackpad_session.pad_get(arguments['<pad_id>'], revision=arguments['--revision'],
+                                           data_format=arguments['--format'])
+            pprint.pprint(resp.content)
 
         if arguments['put']:
-            req_url = api_endpoint + 'pad/' + arguments['<pad_id>'] + '/content'
-            # TODO: Pull file setup out of here, merge with create above.
-            if arguments['<file>'] == '-':
-                f = sys.stdin
-            else:
-                f = open(arguments['<file>'])
-            req_headers = {CONTENT_TYPE_HEADER: CONTENT_TYPE_MAP[arguments['--format']]}
-            print(req_url)
-            print(req_headers)
-            resp = oauth_session.post(req_url, f.read(), headers=req_headers)
+            resp = hackpad_session.pad_put(arguments['<pad_id>'], stream.read(),
+                                           data_format=arguments['--format'])
             pprint.pprint(resp.json())
 
         if arguments['revert']:
-            req_url = api_endpoint + 'pad/' + arguments['<pad_id>'] + '/revert-to' + arguments['--revision']
-            print(req_url)
-            resp = oauth_session.post(req_url).json()
+            resp = hackpad_session.pad_revert(arguments['<pad_id>'], arguments['--revision'])
             pprint.pprint(resp.json())
 
         if arguments['revisions']:
-            req_url = api_endpoint + 'pad/' + arguments['<pad_id>'] + '/revisions'
-            print(req_url)
-            pprint.pprint(oauth_session.get(req_url).json())
+            resp = hackpad_session.pad_revisions(arguments['<pad_id>'])
+            pprint.pprint(resp.json())
 
         if arguments['get_options']:
-            req_url = api_endpoint + 'pad/' + arguments['<pad_id>'] + '/options'
-            print(req_url)
-            pprint.pprint(oauth_session.get(req_url).json())
+            resp = hackpad_session.pad_get_options(arguments['<pad_id>'])
+            pprint.pprint(resp.json())
 
         if arguments['set_option']:
-            req_url = api_endpoint + 'pad/' + arguments['<pad_id>'] + '/options'
-            req_params = {arguments['<setting>']: arguments['<value>']}
-            print(req_url)
-            print(req_params)
-            pprint.pprint(oauth_session.post(req_url, params=req_params).json())
+            resp = hackpad_session.pad_set_option(arguments['<pad_id>'], arguments['<setting>'],
+                                                  arguments['<value>'])
+            pprint.pprint(resp.json())
 
 
 if __name__ == '__main__':
